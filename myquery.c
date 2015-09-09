0d10c8650ee64e28de6917e745f87f003a82a3f6
Gianluca Mazzini <gianluca@mazzini.org>
2015-09-09 20:55:50 +0200

    Update myquery.c

--- myquery.c
+++ myquery.c
@@ -5,18 +5,18 @@
 #include <stdio.h>
 
 int main(){
-  int sockfd,n;
-  struct sockaddr_in servaddr;
-  char recvline[100],ip[100];
-
-  sockfd=socket(AF_INET,SOCK_DGRAM,0);
-  bzero((void *)&servaddr,sizeof(servaddr));
-  servaddr.sin_family = AF_INET;
-  servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
-  servaddr.sin_port=htons(5555);
-
-  for(;;){
-    scanf("%s",ip);
+	int sockfd,n;
+	struct sockaddr_in servaddr;
+	char recvline[100],ip[100];
+	
+	sockfd=socket(AF_INET,SOCK_DGRAM,0);
+	bzero((void *)&servaddr,sizeof(servaddr));
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	servaddr.sin_port=htons(5555);
+	
+	for(;;){
+		scanf("%s",ip);
     if(feof(stdin))break;
     sendto(sockfd,ip,strlen(ip),0,(struct sockaddr *)&servaddr,sizeof(servaddr));
     n=recvfrom(sockfd,recvline,100,0,NULL,NULL);
