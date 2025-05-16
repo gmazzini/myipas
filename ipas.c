7b87d8beb6865cc36bed92caacade1cfb08afa87
Gianluca Mazzini <gianluca@mazzini.org>
2025-05-16 12:18:39 +0200

    Update ipas.c

--- ipas.c
+++ ipas.c
@@ -152,6 +152,8 @@ int main(){
   buf1=strstr(buf1," "); if(buf1==NULL)return 0; else buf1++;
   buf2=strstr(buf1,"\n"); if(buf2==NULL)return 0; *buf2='\0';
   for(i++;i<len;i++)elmv4=elmv4*10+dd[buf1[i]];
+
+  elmv4=100;
   v4=(struct v4 *)malloc(elmv4*sizeof(struct v4));
   if(v4==NULL)return 0;
   for(e=0;;){
