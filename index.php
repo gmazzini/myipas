b622d87fad47420a0543e2472c6b49d19e6dad90
Gianluca Mazzini <gianluca@mazzini.org>
2025-05-19 19:16:15 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,10 @@
 echo "<pre>";
 echo "On the basis of RIPE RIS data sets https://www.ris.ripe.net/\n";
 echo "List of data collectors https://ris.ripe.net/docs/route-collectors\n";
-echo "Real time collection from RRC00 Amsterdam\n";
-echo "data is hourly updated\n";
-echo "retention is 10 days\n";
+echo "Real time collection from RRC11 New York NYIIX\n";
+echo "Data is hourly published\n";
+echo "Retention is 30 days\n";
+echo "Real time query in IPv4 and IPv6 is open with whois, e.g. whois -h ipas.mazzini.org 1.1.1.1\n\n";
 
 function oo($txt,$ff){
   $m=(int)exec("wc -l /home/www/fulltable/$ff");
