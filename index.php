708cc0c16846a69c8215eb1d073bbe12786fc5a4
Gianluca Mazzini <gianluca@mazzini.org>
2025-05-20 06:15:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo "<pre>";
 echo "On the basis of RIPE RIS data sets https://www.ris.ripe.net/\n";
 echo "List of data collectors https://ris.ripe.net/docs/route-collectors\n";
-echo "Real time collection from RRC11 New York NYIIX\n";
+echo "Real time collection\n";
 echo "Data is hourly published\n";
 echo "Retention is 30 days\n";
 echo "Real time query in IPv4 and IPv6 is open with whois, e.g. whois -h ipas.mazzini.org 1.1.1.1\n\n";
