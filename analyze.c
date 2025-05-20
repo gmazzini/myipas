892f9850ba1cc85dacee2e89142479c7b47b08da
Gianluca Mazzini <gianluca@mazzini.org>
2025-05-20 19:34:53 +0200

    Update analyze.c

--- analyze.c
+++ analyze.c
@@ -18,11 +18,27 @@ struct stat {
   uint32_t v4[33];
   uint32_t v6[129];
 };
-long rlmv4,elmv6,elm;
+long elmv4,elmv6,elm;
 
 int main(){
   struct v4 v4;
   struct v6 v6;
+  long i;
+
+  fp=fopen(BKP4FILE,"rb");
+  if(fp==NULL)exit(0);
+  fread(&elmv4,4,1,fp);
+  for(i=0;i<elmv4;i++){
+    fread(&v4,sizeof(struct v4),1,fp);
+  }
+  fclose(fp);
+  
+  fp=fopen(BKP6FILE,"rb");
+  if(fp!=NULL){
+    fread(&elmv6,4,1,fp);
+    fread(v6,sizeof(struct v6),elmv6,fp);
+    fclose(fp);
+  }
   
 
 }
