185478099c7e09dcc26c7cefb96d150c6cd45cbd
Gianluca Mazzini <gianluca@mazzini.org>
2025-05-20 19:41:06 +0200

    Update analyze.c

--- analyze.c
+++ analyze.c
@@ -1,5 +1,6 @@
 #define BKP4FILE "/home/www/fulltable/bkp4.raw"
 #define BKP6FILE "/home/www/fulltable/bkp6.raw"
+#define ASNELM 1000000
 
 struct v4 {
   uint32_t ip;
@@ -17,7 +18,7 @@ struct stat {
   uint32_t asn;
   uint32_t v4[33];
   uint32_t v6[129];
-};
+} *stat;
 long elmv4,elmv6,elm;
 
 int main(){
@@ -25,20 +26,25 @@ int main(){
   struct v6 v6;
   long i;
 
+  stat=(struct stat *)calloc(ASNELM*sizeof(struct stat));
+  if(stat==NULL)exit(0);
   fp=fopen(BKP4FILE,"rb");
   if(fp==NULL)exit(0);
   fread(&elmv4,4,1,fp);
   for(i=0;i<elmv4;i++){
     fread(&v4,sizeof(struct v4),1,fp);
+    myadd(0,v4.asn,v4.cidr);
+  }
+  fclose(fp);
+  fp=fopen(BKP6FILE,"rb");
+  if(fp==NULL)exit(0);
+  fread(&elmv6,4,1,fp);
+  for(i=0;i<elmv6;i++){
+    fread(&v6,sizeof(struct v6),1,fp);
+    myadd(1,v6.asn,v6.cidr);
   }
   fclose(fp);
   
-  fp=fopen(BKP6FILE,"rb");
-  if(fp!=NULL){
-    fread(&elmv6,4,1,fp);
-    fread(v6,sizeof(struct v6),elmv6,fp);
-    fclose(fp);
-  }
   
 
 }
