7713486083e9f8e4c5278dd0afeaa45a868dd71d
Gianluca Mazzini <gianluca@mazzini.org>
2025-06-04 10:43:34 +0200

    Update analyze.c

--- analyze.c
+++ analyze.c
@@ -3,20 +3,7 @@
 #include <stdlib.h>
 #include <time.h>
 #define BGPFILE "/home/www/fulltable/bgp.raw"
-
-#define BKP4FILE "/home/www/fulltable/bkp4.raw"
-#define BKP6FILE "/home/www/fulltable/bkp6.raw"
-#define HASHELM 16777216
-
-uint32_t myhash(uint32_t asn){
-  uint32_t x=asn;
-  x^=x>>16;
-  x*=0x85ebca6b;
-  x^=x>>13;
-  x*=0xc2b2ae35;
-  x^=x>>16;
-  return x&0x00FFFFFF;
-}
+#define ASN 200000 
 
 int main(){
   struct v4 {
@@ -32,55 +19,38 @@ int main(){
     uint32_t ts;
   } v6;
   struct stat {
-    uint32_t asn;
-    uint32_t v4[33];
-    uint32_t v6[129];
-  } **ptr;
+    uint32_t v4[17]; // 8 to 24
+    uint32_t v6[33];   // 16 to 48
+  } *stat;
   uint64_t tot;
   uint8_t j;
   FILE *fp;
   uint32_t i,vv[100],q,elmv4,elmv6;
   time_t tr;
 
-  ptr=(struct stat **)malloc(HASHELM*sizeof(struct stat *));
-  if(ptr==NULL)exit(0);
-  for(i=0;i<HASHELM;i++)ptr[i]=NULL;
+  stat=(struct stat *)malloc(ASN*sizeof(struct stat));
+  if(stat==NULL)exit(0);
+  for(i=0;i<ASN;i++){
+    for(j=0;j<17;j++)stat[i].v4[j]=0;
+    for(j=0;j<33;j++)stat[i].v6[j]=0;
+  }
   for(i=0;i<100;i++)vv[i]=0;
   tr=time(NULL);
-  
-  fp=fopen(BKP4FILE,"rb");
+  fp=fopen(BGPFILE,"rb");
   if(fp==NULL)exit(0);
-  fread(&elmv4,4,1,fp);
-  for(i=0;i<elmv4;i++){
+  fread(&nv4,4,1,fp);
+  fread(&nv6,4,1,fp);
+  for(i=0;i<nv4;i++){
     fread(&v4,sizeof(struct v4),1,fp);
+    if(v4.asn==0||v4.cidr<8||v4.cidr>24)continue;
     vv[(tr-v4.ts)/86400]++;
-    q=myhash(v4.asn);
-    if(ptr[q]==NULL){
-      ptr[q]=(struct stat *)malloc(sizeof(struct stat));
-      if(ptr[q]==NULL)exit(0);
-      for(j=0;j<33;j++)ptr[q]->v4[j]=0;
-      for(j=0;j<129;j++)ptr[q]->v6[j]=0;
-      ptr[q]->asn=v4.asn;
-    }
-    ptr[q]->v4[v4.cidr]++;
+    stat[v4.asn].v4[v4.cidr-8]++;
   }
-  fclose(fp);
-
-  fp=fopen(BKP6FILE,"rb");
-  if(fp==NULL)exit(0);
-  fread(&elmv6,4,1,fp);
-  for(i=0;i<elmv6;i++){
+  for(i=0;i<nv6;i++){
     fread(&v6,sizeof(struct v6),1,fp);
+    if(v6.asn==0||v6.cidr<16||v6.cidr>48)continue;
     vv[(tr-v6.ts)/86400]++;
-    q=myhash(v6.asn);
-    if(ptr[q]==NULL){
-      ptr[q]=(struct stat *)malloc(sizeof(struct stat));
-      if(ptr[q]==NULL)exit(0);
-      for(j=0;j<33;j++)ptr[q]->v4[j]=0;
-      for(j=0;j<129;j++)ptr[q]->v6[j]=0;
-      ptr[q]->asn=v6.asn;
-    }
-    ptr[q]->v6[v6.cidr]++;
+    stat[v6.asn].v6[v6.cidr-16]++;
   }
   fclose(fp);
 
