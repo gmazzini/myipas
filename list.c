48a725848c40da7bd1ed99dcf7c1bd7bb6ad04f8
Gianluca Mazzini <gianluca@mazzini.org>
2025-06-04 06:53:41 +0200

    Update list.c

--- list.c
+++ list.c
@@ -41,7 +41,7 @@ int cmp_v6(const void *a, const void *b){
 }
 
 int main(){
-  uint32_t i,j,q,c4[33],c6[129],ip4,nv4,nv6;
+  uint32_t i,j,q,c4[33],c6[129],ip4,nv4,nv6,anv4,anv6;
   uint8_t a[4];
   uint16_t b[4];
   uint64_t ip6;
@@ -66,22 +66,24 @@ int main(){
   printf("run %lu %lu\n",nv4,nv6);
 
   for(i=0;i<33;i++)c4[i]=0;
-  for(i=0;i<nv4;i++)c4[v4[i].cidr]++;
+  for(anv4=0,i=0;i<nv4;i++)if(v4[i].cidr>0&&v4[i].asn>0){c4[v4[i].cidr]++; anv4++;}
   fp=fopen(V4FILE,"wt");
-  fprintf(fp,"# v4_tot: %lu\n",nv4);
+  fprintf(fp,"# v4_tot: %lu\n",anv4);
   for(i=0;i<33;i++)if(c4[i]>0)fprintf(fp,"# v4_cidr%d: %lu\n",i,c4[i]);
   for(i=0;i<nv4;i++){
+    if(v4[i].cidr==0||v4[i].asn==0)continue;
     for(ip4=v4[i].ip,j=0;j<4;j++){a[j]=ip4&0xff; ip4>>=8;}
     fprintf(fp,"%d.%d.%d.%d",a[3],a[2],a[1],a[0]);
     fprintf(fp,"/%d,%lu\n",v4[i].cidr,v4[i].asn);
   }
   fclose(fp);
   for(i=0;i<129;i++)c6[i]=0;
-  for(i=0;i<nv6;i++)c6[v6[i].cidr]++;
+  for(anv6=0,i=0;i<nv6;i++)if(v6[i].cidr>0&&v6[i].asn>0){c6[v6[i].cidr]++; anv6++;}
   fp=fopen(V6FILE,"wt");
   fprintf(fp,"# v6_tot: %lu\n",nv6);
   for(i=0;i<129;i++)if(c6[i]>0)fprintf(fp,"# v6_cidr%d: %lu\n",i,c6[i]);
   for(i=0;i<nv6;i++){
+    f(v6[i].cidr==0||v6[i].asn==0)continue;
     for(ip6=v6[i].ip,q=0;q<64;q++)if(ip6&1)break; else ip6>>=1;
     for(ip6=v6[i].ip,j=0;j<4;j++){b[j]=ip6&0xffff; ip6>>=16;}
     if(q>=48)fprintf(fp,"%x::",b[3]);
